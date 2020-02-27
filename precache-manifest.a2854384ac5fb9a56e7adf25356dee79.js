4111305c9170a5254efadc000a3add92a2160d7b
Aris Papadopoulos <aris.ppd@gmail.com>
2020-02-27 17:50:02 +0200

    Initial commit
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

---
+++ precache-manifest.a2854384ac5fb9a56e7adf25356dee79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45243161569916a6052c76edd8df7c59",
+    "url": "/react-resume-template/index.html"
+  },
+  {
+    "revision": "23ed731fd1547eb8514e",
+    "url": "/react-resume-template/static/css/main.26290c86.chunk.css"
+  },
+  {
+    "revision": "38351ce40247e8a6f769",
+    "url": "/react-resume-template/static/js/2.ad0f70a9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-resume-template/static/js/2.ad0f70a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23ed731fd1547eb8514e",
+    "url": "/react-resume-template/static/js/main.db342856.chunk.js"
+  },
+  {
+    "revision": "35906921750a6d31c6c8",
+    "url": "/react-resume-template/static/js/runtime-main.228f9154.js"
+  }
+]);
